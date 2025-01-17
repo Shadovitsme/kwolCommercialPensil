3a459a1214e0cb08c3cfdf84cc7b33f4b7a01ed4
ukki <iu.cashirskaia@yandex.ru>
2025-01-17 17:51:22 +0300

    addCardButtonComponent

--- src/addRefPage.vue
+++ src/addRefPage.vue
@@ -3,6 +3,7 @@ import { ref, watch } from 'vue'
 import LinkPicBlock from './components/linkPicBlock.vue'
 import YellowButton from './components/yellowButton.vue'
 import router from './router'
+import AddCardButton from './components/addCardButton.vue'
 
 function changeInputCount(e) {
   router.replace({ path: '/brief_com/thanksPage' })
@@ -12,14 +13,14 @@ function changeInputCount(e) {
   <div class="flex px-[22px] md:px-[100px]">
     <div class="md:mx-auto w-full max-w-[1920px]">
       <form id="page3" class="w-full h-full">
-        <h1 class="H1 Text pb-10 uppercase">Выберите комнаты</h1>
-        <p class="p3 Text pb-10">
+        <h1 class="H1 Text pb-10 uppercase">ДОПОЛНИТЕЛЬНЫЕ ПОЖЕЛАНИЯ</h1>
+        <p class="p3 Text ">
           Если у вас есть референсы/примеры того, что вы бы хотели видеть, прикрепите их ниже
-          ссылками или изображениями
+          ссылками или изображениями.
         </p>
+        <p class="p3 text-Text pb-10">Вы можете добавить <p class="speciall font-[28px] inline-block">до 10 референсов.</p></p>
 
-        <LinkPicBlock />
-
+<AddCardButton></AddCardButton>
         <div class="w-full flex md:justify-end mt-9 md:mt-12">
           <YellowButton
             :onclick="changeInputCount"
---
+++ src/components/addCardButton.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+<template>
+  <button
+    type="button"
+    class="h-[10.625rem] md:h-[25rem] bg-DarkAccent border-[0.0625rem] border-Accent w-full md:w-[16.75rem] rounded-[0.625rem] flex hover:bg-background active:bg-DarkAccent"  >
+    <div class="plus my-auto mx-auto h-20 w-20"></div>
+  </button>
+</template>
+
+<style>
+.plus {
+  background: no-repeat
+    url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAiIGhlaWdodD0iODAiIHZpZXdCb3g9IjAgMCA4MCA4MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTIwIDQwSDYwIiBzdHJva2U9IiNGNUYyRUMiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPHBhdGggZD0iTTQwIDYwVjIwIiBzdHJva2U9IiNGNUYyRUMiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+');
+}
+</style>
